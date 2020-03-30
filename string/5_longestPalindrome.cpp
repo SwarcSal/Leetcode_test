5e72b32e7199cc1a3289b79eaea8de40c2c2e811
youngchany <372838869@qq.com>
2020-03-30 23:03:37 +0800

    docs: test

--- string/5_longestPalindrome.cpp
+++ string/5_longestPalindrome.cpp
@@ -4,7 +4,7 @@
  * @Author: youngchany
  * @Date: 2020-03-30 21:53:34
  * @LastEditors: youngchany
- * @LastEditTime: 2020-03-30 22:57:35
+ * @LastEditTime: 2020-03-30 23:03:13
  */
 
 /*
@@ -29,7 +29,7 @@
 //内存消耗 :102.1 MB, 在所有 C++ 提交中击败了35.49%的用户
 /*
 遍历字符串中每个字符，找到以当前字符为中心的最长回文串，并记录start和end
-遍历完后，得到最长回文串
+遍历完后，得到最长回文串 
 */
 class Solution {
 public:
