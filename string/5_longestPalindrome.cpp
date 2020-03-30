45acf4fe3be1e58bedd2d1bb69cfd52ccdbd0571
youngchany <372838869@qq.com>
2020-03-30 22:22:18 +0800

    code: 最长回文串

---
+++ string/5_longestPalindrome.cpp
@@ -0,0 +1,96 @@
+/*
+ * @Descripttion: 
+ * @version: v0.1
+ * @Author: youngchany
+ * @Date: 2020-03-30 21:53:34
+ * @LastEditors: youngchany
+ * @LastEditTime: 2020-03-30 22:21:49
+ */
+
+/*
+给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+
+示例 1：
+
+输入: "babad"
+输出: "bab"
+注意: "aba" 也是一个有效答案。
+示例 2：
+
+输入: "cbbd"
+输出: "bb"
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/longest-palindromic-substring
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+//执行用时 :112 ms, 在所有 C++ 提交中击败了51.52%的用户
+//内存消耗 :102.1 MB, 在所有 C++ 提交中击败了35.49%的用户
+class Solution {
+public:
+    int expandAroundCenter(string s, int left, int right)
+    {
+        int L = left, R = right;
+        while (L >= 0 && R < s.length() && s.at(L) == s.at(R)) {
+            L--;
+            R++;
+        }
+        return R - L - 1;
+    }
+
+    string longestPalindrome(string s) {
+        if (s.length() < 1)
+            return "";
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); ++i) {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substr(start, end-start+1);
+    }
+};
+
+string stringToString(string input) {
+    assert(input.length() >= 2);
+    string result;
+    for (int i = 1; i < input.length() -1; i++) {
+        char currentChar = input[i];
+        if (input[i] == '\\') {
+            char nextChar = input[i+1];
+            switch (nextChar) {
+                case '\"': result.push_back('\"'); break;
+                case '/' : result.push_back('/'); break;
+                case '\\': result.push_back('\\'); break;
+                case 'b' : result.push_back('\b'); break;
+                case 'f' : result.push_back('\f'); break;
+                case 'r' : result.push_back('\r'); break;
+                case 'n' : result.push_back('\n'); break;
+                case 't' : result.push_back('\t'); break;
+                default: break;
+            }
+            i++;
+        } else {
+          result.push_back(currentChar);
+        }
+    }
+    return result;
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        string s = stringToString(line);
+        
+        string ret = Solution().longestPalindrome(s);
+
+        string out = (ret);
+        cout << out << endl;
+    }
+    return 0;
+}
